c941e4bd0afa71160cb882b92ce8be3ab6855f9e
Ann Keosykhao <72222180+keosykhao@users.noreply.github.com>
2021-05-14 08:33:45 -0400

    .

--- movie/src/components/Searchform.js
+++ movie/src/components/Searchform.js
@@ -11,14 +11,14 @@ export default function SearchMovies(){
         e.preventDefault();
         console.log("submitting");
         
-        const query = "Jurassic Park";
+        const query = "Star Wars";
         
         const url = `https://api.themoviedb.org/3/search/movie?api_key=fb24b074b411da128b57c4eae1a84a0c&language=en-US&query=${query}&page=1&include_adult=false`;
         
         try {
             const res = await fetch(url);
             const data  = await res.json();
-            console.log(data.results);
+            setMovies(data.results);
         }catch(err){
             console.error(err);
         }
@@ -33,24 +33,24 @@ export default function SearchMovies(){
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}/>
             <button className="button" type="submit">Search</button>
-                <div className="card-list">
-                    {movies.map(movie => (
-                        <div className= "card">
-                            <img className="card--image"
-                            src={{`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+            </form>
+            <div className="card-list">
+                {movies.filter(movie => movie.poster_path).map(movie => (
+                    <div className="card" key={movie.id}>
+                        <img className="card--image"
+                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
                             alt={movie.title + ' poster'}
                             />
-
+                        <div className="card--content">
+                        <h3 className="card--title">{movie.title}</h3>
+                        <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                        <p><small>RATING: {movie.vote_average}</small></p>
+                        <p className="card--desc">{movie.overview}</p>
                         </div>
 
-                    ))}
-
-
-                </div>
-
-
-
-        </form>
+                    </div>
+                ))}
+            </div>     
 
         </>
     )
